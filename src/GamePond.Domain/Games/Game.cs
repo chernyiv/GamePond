84bbf5728448858a34b04e96d61a4154fd9e9210
Vitaliy Chernyi <chernyiv@github.com>
2026-07-31 01:19:29 +0300

    Add Game record

---
+++ src/GamePond.Domain/Games/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace GamePond.Domain.Games;
+
+public sealed record Game()
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public DateOnly? ReleaseDate { get; set; }
+    public required DateTimeOffset CreationDate { get; init; }
+}

55c20a7031a53d6f06a1553a183c306d9ecb57bf
Vitaliy Chernyi <chernyiv@github.com>
2026-07-31 01:53:59 +0300

    Add game dto, commands and validators

---
+++ src/GamePond.Application/Games/Validators/UpdateGameCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using GamePond.Application.Games.Models;
+
+namespace GamePond.Application.Games.Validators;
+
+public class UpdateGameCommandValidator : AbstractValidator<UpdateGameCommand>
+{
+    public UpdateGameCommandValidator()
+    {
+        RuleFor(g => g.Title).NotEmpty().MaximumLength(200);
+        RuleFor(g => g.Description).MaximumLength(2000);
+    }
+}

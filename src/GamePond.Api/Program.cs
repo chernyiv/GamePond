6a01920a541a7b1b0a05481a0a9950ccf1a1c87f
Vitaliy Chernyi <chernyiv@github.com>
2026-08-04 01:20:39 +0300

    Reconfigure Program.cs and http files

--- src/GamePond.Api/Program.cs
+++ src/GamePond.Api/Program.cs
@@ -1,12 +1,30 @@
+using FluentValidation;
+using GamePond.Application.Games.Models;
+using GamePond.Application.Games.Repositories;
+using GamePond.Application.Games.Services;
+using GamePond.Application.Games.Validators;
+using GamePond.Infrastructure.Games.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddControllers();
+
+builder.Services.AddValidatorsFromAssemblyContaining<CreateGameCommandValidator>();
+
+builder.Services.AddSingleton<
+    IGameRepository,
+    InMemoryGameRepository>();
+
+builder.Services.AddScoped<
+    IGameService,
+    GameService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -14,28 +32,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
